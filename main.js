42af2ba2be07ae2344c10f27dee5242fe88fb190
Brian Gao <bhgao20014@gmail.com>
2021-06-26 19:17:47 -0400

    Did a little more of the logic part of the game

--- main.js
+++ main.js
@@ -1,37 +1,19 @@
-function computerPlay() {   
-    const possibleSelections = ["rock", "paper", "scissors"]; 
-    // 0-2
-    let randomIndex = Math.floor(Math.random() * 3); 
-    // random array element
-    return possibleSelections[randomIndex]; 
-}
+let btns = document.querySelectorAll('.btn');
 
-function userPlay() { 
-    const validAnswers = ["rock", "paper", "scissors"];
-    // user's input 
-    let userSelection; 
-    // loops until user cancels or puts valid input
-    while (!validAnswers.includes(userSelection)) {
-        userSelection = prompt("Choose 'rock', 'paper', or 'scissors'"); 
-        // checking if user pressed "Cancel"
-        if (userSelection == null) {
-            return null; 
-        }
-        // to make input case-insensitive
-        userSelection = userSelection.toLowerCase(); 
-    }
-    // returns valid input
-    return userSelection; 
-}
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e);
+        let userSelection = e.toElement.className;
+        console.log(userSelection);
+        let result = playRound(userSelection, computerPlay());
+        updateScore(result);
+        checkScore();
+    })
+})
 
-// computes one round of Rock-Paper-Scissors
 function playRound(userSelection, computerSelection) {
-    // if user cancelled
-    if (userSelection == null) {
-        return null;
-    }
     // if user & computer choose the same
-    else if (userSelection == computerSelection) {
+    if (userSelection == computerSelection) {
         return "Tie"; 
     }
     // if user beats computer
@@ -46,50 +28,14 @@ function playRound(userSelection, computerSelection) {
     }
 }
 
-// parameter == # of rounds of Rock-Paper-Scissors
-function playGame(counter) {
-    let userScore = 0; 
-    let computerScore = 0; 
-    let result; // result of one round
-
-    // loops "counter" times
-    for (let i = 0; i < counter; i++) {
-        result = playRound(userPlay(), computerPlay()); // one round/iteration
-        // To determine results of one round
-        switch (result) {
-            // User cancelled
-            case null:
-                console.log("User Cancelled");
-                return;
-            // User tied round
-            case "Tie":
-                userScore++;
-                computerScore++;
-                break;
-            // User won round
-            case "Win":
-                userScore++;
-                break;
-            // User lost round
-            case "Lose":
-                computerScore++;
-                break;
-        }
-    } // finals results are in; comparison time!
-    // To determine who won overall
-    switch (true) {
-        // User tied
-        case userScore == computerScore:
-            console.log("Fucking tie I quit");
-            break;
-        // User won
-        case userScore > computerScore:
-            console.log("Let's fucking go, I take the dubs"); 
-            break;
-        // User lost
-        case userScore < computerScore:
-            console.log("Bruh this is rigged");
-            break;
-    }
-    return; // after logging result, return no value
+function computerPlay() {   
+    const possibleSelections = ["rock", "paper", "scissors"]; 
+    // 0-2
+    let randomIndex = Math.floor(Math.random() * 3); 
+    // random array element
+    return possibleSelections[randomIndex]; 
 }
+
+
+
+
