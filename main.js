e42f8b7cf62133990cf1bd5ae726429ff29435d0
Brian Gao <bhgao20014@gmail.com>
2021-06-30 16:04:48 -0400

    Working on the audio that plays each round depending on the result

--- main.js
+++ main.js
@@ -6,18 +6,6 @@ function main() {
     btns.forEach((btn) => {btn.addEventListener('click', playGame)});
 }
 
-function getMusicPermission() {
-    document.getElementById('yes').addEventListener('click', () => {
-        document.getElementById('background-music').play();
-        document.getElementById('music-permission').style.visibility = 'hidden';
-    })
-    document.getElementById('no').addEventListener('click', () => {
-        document.getElementById('music-permission').style.visibility = 'hidden';
-    })
-    
-
-}
-
 function playGame(e) {
     let userSelection = e.toElement.className.split(' ')[0];
     let computerSelection = computerPlay();
@@ -27,6 +15,102 @@ function playGame(e) {
     if (checkScore() == "Done") {
         endGame();
     }
+}
+
+function endGame() {
+    let btns = document.querySelectorAll('div#rps-icons > button');
+    btns.forEach((btn) => {btn.removeEventListener('click', playGame)});
+    const tmpRef = document.getElementById('score-display');
+    const restart = document.getElementById('restart');
+    restart.textContent = 'Restart?';
+    restart.style.visibility = 'visible';
+    restart.addEventListener('click', reset)
+}
+
+function reset() {
+    document.getElementById('user-tally').textContent = "0";
+    document.getElementById('computer-tally').textContent = "0";
+    document.getElementById('display-message').textContent = '';
+    restart.textContent = '';
+    restart.style.visibility = 'hidden';
+    restart.removeEventListener('click', reset)
+    main();
+}
+
+function checkScore() {
+    let userScore = +document.getElementById('user-tally').textContent;
+    let compScore = +document.getElementById('computer-tally').textContent;
+    const displayMessage = document.getElementById('display-message');
+    if (userScore == 5 && compScore == 5) {
+        displayMessage.textContent = 'It\'s a tie :|';
+        return "Done";
+    }
+    else if (userScore == 5) {
+        displayMessage.textContent = 'You win :)';
+        document.getElementById('win-game-audio').play();
+        return "Done";
+    }
+    else if (compScore == 5) {
+        displayMessage.textContent = 'You lose :(';
+        document.getElementById('lose-game-audio').play();
+        return "Done";
+    }
+
+    
+}
+
+function updateScore(result) {
+    const displayMessage = document.getElementById('display-message');
+    if (result == 'Tie') {
+        const userScore = document.getElementById('user-tally');
+        let tmpUserScore = +userScore.textContent;
+        userScore.textContent = ++tmpUserScore; 
+
+        const CompScore = document.getElementById('computer-tally');
+        let tmpCompScore = +CompScore.textContent;
+        CompScore.textContent = ++tmpCompScore; 
+
+        displayMessage.textContent = 'Tie!';
+    }
+
+    else if (result == 'Win') {
+        const userScore = document.getElementById('user-tally');
+        let tmpUserScore = +userScore.textContent;
+        userScore.textContent = ++tmpUserScore; 
+
+        displayMessage.textContent = 'Win!';
+    }
+    else {
+        const CompScore = document.getElementById('computer-tally');
+        let tmpCompScore = +CompScore.textContent;
+        CompScore.textContent = ++tmpCompScore; 
+        displayMessage.textContent = 'Lose!';
+    }
+}
+
+function playRound(userSelection, computerSelection) {
+    // if user & computer choose the same
+    if (userSelection == computerSelection) {
+        return "Tie"; 
+    }
+    // if user beats computer
+    else if (userSelection == "rock" && computerSelection == "scissors"|| 
+            userSelection == "scissors" && computerSelection == "paper" ||
+            userSelection == "paper" && computerSelection == "rock") {
+        return "Win"; 
+    }
+    // if computer beats user
+    else {
+        return "Lose"; 
+    }
+}
+
+function computerPlay() {   
+    const possibleSelections = ["rock", "paper", "scissors"]; 
+    // 0-2
+    let randomIndex = Math.floor(Math.random() * 3); 
+    // random array element
+    return possibleSelections[randomIndex]; 
 }
 
 function displayIcons(userSelection, computerSelection, result) {
@@ -73,104 +157,14 @@ function displayIcons(userSelection, computerSelection, result) {
             userExpression.setAttribute('src', 'imgs/user-lose.png');
             break;
     }
-
-
 }
 
-function endGame() {
-    let btns = document.querySelectorAll('div#rps-icons > button');
-    btns.forEach((btn) => {btn.removeEventListener('click', playGame)});
-    const tmpRef = document.getElementById('score-display');
-    const restart = document.getElementById('restart');
-    restart.textContent = 'Restart?';
-    restart.style.visibility = 'visible';
-    restart.addEventListener('click', reset)
-}
-
-function reset() {
-    document.getElementById('user-tally').textContent = "0";
-    document.getElementById('computer-tally').textContent = "0";
-    document.getElementById('display-message').textContent = '';
-    restart.textContent = '';
-    restart.style.visibility = 'hidden';
-    restart.removeEventListener('click', reset)
-    main();
-}
-
-function checkScore() {
-    let userScore = +document.getElementById('user-tally').textContent;
-    let compScore = +document.getElementById('computer-tally').textContent;
-    const displayMessage = document.getElementById('display-message');
-    if (userScore == 5 && compScore == 5) {
-        displayMessage.textContent = 'It\'s a tie :|';
-        document.getElementById('win-round-audio').play();
-        return "Done";
-    }
-    else if (userScore == 5) {
-        displayMessage.textContent = 'You win :)';
-        document.getElementById('win-game-audio').play();
-        return "Done";
-    }
-    else if (compScore == 5) {
-        displayMessage.textContent = 'You lose :(';
-        document.getElementById('lose-game-audio').play();
-        return "Done";
-    }
-
-    
-}
-
-function updateScore(result) {
-    const displayMessage = document.getElementById('display-message');
-    if (result == 'Tie') {
-        const userScore = document.getElementById('user-tally');
-        let tmpUserScore = +userScore.textContent;
-        userScore.textContent = ++tmpUserScore; 
-
-        const CompScore = document.getElementById('computer-tally');
-        let tmpCompScore = +CompScore.textContent;
-        CompScore.textContent = ++tmpCompScore; 
-
-        displayMessage.textContent = 'Tie!';
-    }
-
-    else if (result == 'Win') {
-        const userScore = document.getElementById('user-tally');
-        let tmpUserScore = +userScore.textContent;
-        userScore.textContent = ++tmpUserScore; 
-
-        displayMessage.textContent = 'Win!';
-    }
-    else {
-        const CompScore = document.getElementById('computer-tally');
-        let tmpCompScore = +CompScore.textContent;
-        CompScore.textContent = ++tmpCompScore; 
-
-        displayMessage.textContent = 'Lose!';
-    }
-}
-
-function playRound(userSelection, computerSelection) {
-    // if user & computer choose the same
-    if (userSelection == computerSelection) {
-        return "Tie"; 
-    }
-    // if user beats computer
-    else if (userSelection == "rock" && computerSelection == "scissors"|| 
-            userSelection == "scissors" && computerSelection == "paper" ||
-            userSelection == "paper" && computerSelection == "rock") {
-        return "Win"; 
-    }
-    // if computer beats user
-    else {
-        return "Lose"; 
-    }
-}
-
-function computerPlay() {   
-    const possibleSelections = ["rock", "paper", "scissors"]; 
-    // 0-2
-    let randomIndex = Math.floor(Math.random() * 3); 
-    // random array element
-    return possibleSelections[randomIndex]; 
+function getMusicPermission() {
+    document.getElementById('yes').addEventListener('click', () => {
+        document.getElementById('background-music').play();
+        document.getElementById('music-permission').style.visibility = 'hidden';
+    })
+    document.getElementById('no').addEventListener('click', () => {
+        document.getElementById('music-permission').style.visibility = 'hidden';
+    })
 }
